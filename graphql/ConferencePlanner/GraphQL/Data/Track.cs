dcc48172b6e4b0aaece5c4e441c54c9b0829d1cb
Chau Than <simplesoul.chau@gmail.com>
2024-07-24 17:35:11 +0700

    Session 5: Understanding middleware

--- graphql/ConferencePlanner/GraphQL/Data/Track.cs
+++ graphql/ConferencePlanner/GraphQL/Data/Track.cs
@@ -8,6 +8,7 @@ namespace ConferencePlanner.GraphQL.Data
 
         [Required]
         [StringLength(200)]
+        [UseUpperCase]
         public string? Name { get; set; }
 
         public ICollection<Session> Sessions { get; set; } = [];
