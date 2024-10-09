b7d185a09da1abb9518478d429cacd842881c8a0
Chau Than <simplesoul.chau@gmail.com>
2024-10-09 11:32:27 +0700

    Add models

---
+++ TodoApp/StandaloneConsole/src/TodoApp.Console/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿namespace TodoApp.Console.Models;
+
+public class TodoItem
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Content { get; set; } = string.Empty;
+
+    public bool Done { get; set; }
+
+    public DateTimeOffset CreatedAt { get; set; }
+
+    public Guid? ListId { get; set; }
+}
---
+++ TodoApp/StandaloneConsole/src/TodoApp.Console/Models/TodoList.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoApp.Console.Models;
+
+public class TodoList
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string Title { get; set; } = string.Empty;
+}
