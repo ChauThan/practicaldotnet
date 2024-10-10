987b39a6d61f64bbd22098fcfd14a9c87cdb55fe
Chau Than <simplesoul.chau@gmail.com>
2024-10-10 13:42:58 +0700

    Add DI

---
+++ TodoApp/StandaloneConsole/src/TodoApp.Console/App.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApp.Console;
+
+public class App
+{
+    public async Task RunAsync(string[] args)
+    {
+        System.Console.WriteLine("Hello world");
+
+        await Task.CompletedTask;
+    }
+}
--- TodoApp/StandaloneConsole/src/TodoApp.Console/Program.cs
+++ TodoApp/StandaloneConsole/src/TodoApp.Console/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using TodoApp.Console;
+
+var services = new ServiceCollection();
+
+// create service provider
+var serviceProvider = services.BuildServiceProvider();
+
+// start App
+var app = ActivatorUtilities.CreateInstance<App>(serviceProvider);
+if (app is not null)
+{
+    await app.RunAsync(args);
+}
